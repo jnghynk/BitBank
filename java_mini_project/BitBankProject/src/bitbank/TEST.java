dcddddc8894d488f1f813ac077d1993c2a17409c
KIM <71624723+jnghynkm@users.noreply.github.com>
2020-11-02 14:59:06 +0900

    Update TEST.java

--- java_mini_project/BitBankProject/src/bitbank/TEST.java
+++ java_mini_project/BitBankProject/src/bitbank/TEST.java
@@ -1,31 +1,12 @@
 package bitbank;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TEST {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(ShortLoanInterest(loanAmount));
+	// https://github.com/jnghynkm/myaiaclass/tree/main/java_project/BitBank
+	// 수정본 깃허브 주소입니다.
+	// 추후 업뎃 예정
 		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
-	
-	static long loanAmount = 100;
-	static long loanBalance = 0;
-	static long interest = 0;
-	
-	// 단기 대출 이자 반환 메서드
-	static long ShortLoanInterest(long loanAmount) {
-		interest = (int)(loanAmount*0.07/12*100);
-		return interest;
 	}
 }
